107c191508fd676eecd1e2dda33b74369a5834a8
Biyuan Ding <dby@megvii.com>
2015-11-26 18:24:30 +0800

    Fix typos

--- install.sh
+++ install.sh
@@ -13,27 +13,28 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 if [ ! -f ~/.vimrc.before.local ]; then
     cat << 'EOF_BEFORE_LOCAL' > ~/.vimrc.before.local
 " This file is loaded before the configuration in .vimrc is loaded.
-" Modify g:tdvimrc-features to enable more features.
+" Modify g:tdvimrc_features to enable more features.
 " After enabling or disabling featuers, you need to run :PluginClean and
-" :PluginInstall in VIM to remove and add the plugins in the features you enable.
+" :PluginInstall in VIM to remove and add the plugins in the features you enabled.
 "
-" let g:tdvimrc-features = ['golang', 'tmux', 'html']
+" All the extra features that tdvimrc supports:
+" let g:tdvimrc_features = ["golang", "tmux", "html"]
 "
-let g:tdvimrc-features = []
+let g:tdvimrc_features = []
 EOF_BEFORE_LOCAL
 fi
 
 if [ ! -f ~/.vimrc.after.local ]; then
     cat << 'EOF_AFTER_LOCAL' > ~/.vimrc.after.local
 " This file is loaded after the configuration in .vimrc is loaded.
-" You can override the configurations in .vimrc by editing this file.
+" Override the configurations in .vimrc by editing this file.
 "
 EOF_AFTER_LOCAL
 fi
 
 if [ ! -f ~/.vimrc.bundle.local ]; then
     cat << 'EOF_BUNDLE_LOCAL' > ~/.vimrc.bundle.local
-" Configure your plugins in this file so that Vundle manages your plugins.
+" Add your plugins in this file so that Vundle manages them for you.
 " After adding or removing your plugins, you need to run :PluginInstall to
 " install new plugins and :PluginClean to remove unneeded plugins in VIM.
 "
