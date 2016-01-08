3d8ecd84e350b9b719ac86022483f9db140229ba
Biyuan Ding <dby@megvii.com>
2016-01-08 14:39:21 +0800

    Add installation for tmux

--- install.sh
+++ install.sh
@@ -10,18 +10,22 @@ backup () {
 }
 
 # Step 1: clone the vimrc to local home directory.
+echo "[1/7] Cloning td-vimrc to disk ..."
 backup ~/.td-vimrc
 git clone https://github.com/thomasding/td-vimrc.git ~/.td-vimrc
 
 # Step 2: link the vimrc to .vimrc file.
+echo "[2/7] Linking td-vimrc to ~/.vimrc ..."
 backup ~/.vimrc
 ln -s ~/.td-vimrc/vimrc ~/.vimrc
 
 # Step 3: clone vundle.
+echo "[3/7] Cloning Vundle to disk ..."
 backup ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Step 4: create .vimrc.before.local .vimrc.after.local and .vimrc.plugin.local
+echo "[4/7] Creating ~/.vimrc.before.local ~/.vimrc.after.local and ~/.vimrc.bundle.local"
 if [ ! -f ~/.vimrc.before.local ]; then
     cat << 'EOF_BEFORE_LOCAL' > ~/.vimrc.before.local
 " This file is loaded before the configuration in .vimrc is loaded.
@@ -32,7 +36,7 @@ if [ ! -f ~/.vimrc.before.local ]; then
 " All the extra features that tdvimrc supports:
 " let g:tdvimrc_features = ["golang", "tmux", "web"]
 "
-let g:tdvimrc_features = []
+let g:tdvimrc_features = ["tmux"]
 EOF_BEFORE_LOCAL
 fi
 
@@ -54,3 +58,20 @@ if [ ! -f ~/.vimrc.after.local ]; then
 EOF_AFTER_LOCAL
 fi
 
+# Step 5: install plugins.
+echo "[5/7] Installing plugins ..."
+vim +PluginInstall +qa
+
+# Step 6: create tmux navigator file.
+echo "[6/7] Setting up tmux navigator ..."
+echo "\nsource-file ~/.td-vimrc/tmux-navigator.conf\n" >> ~/.tmux.conf
+
+# Step 7: in tmux, create tmuxline.
+if [[ ! -z $TMUX ]]
+then
+    echo "[7/7] Found tmux. Setting up tmuxline."
+    vim "+TmuxlineSnapshot ~/.tmuxline.conf" +qa
+    echo "\nsource-file ~/.tmuxline.conf\n" >> ~/.tmux.conf
+else
+    echo "[7/7] No tmux found, skip setting up tmuxline."
+fi
